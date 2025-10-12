7a698b1d58d387d585952b178586c2beab91e3a2
Mario Ervedosa <mario.ervedosa@gmail.com>
2025-10-12 01:22:18 +0100

    feat: poetry

--- src/config.py
+++ src/config.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 
 from dotenv import load_dotenv
-from loguru import logger
 
 # Load environment variables from .env file if it exists
 load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 DATABASE_DIR = DATA_DIR / "database"
